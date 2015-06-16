4d6cedd8ce056c7f8b15a84226be33168e696d2e
ycartc <tracymcaruso@gmail.com>
2015-06-16 09:09:25 -0600

    wip

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -5,8 +5,5 @@ Instagram.configure do |config|
   config.client_secret = ENV["INSTAGRAM_SECRET"]
 end
 
-production:
-CALLBACK_URL = "https://the-street-art-project.herokuapp.com/auth/instagram/callback"
 
-development:
-CALLBACK_URL = "http://localhost:3000/auth/instagram/callback"
+# CALLBACK_URL = "http://localhost:3000/auth/instagram/callback"
