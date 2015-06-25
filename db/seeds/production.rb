7da7250ed1b439c1358b5199f858f29bfa1f6732
ycartc <tracymcaruso@gmail.com>
2015-06-24 21:18:20 -0600

    add admin

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -10,6 +10,10 @@ class Seed
     create_murals
   end
 
+  def user_create
+    User.create(full_name: "Tracy Caruso", user_name: "tracymcaruso", email: "tracy@gmail.com", provider: "instagram", token: "1917762378.26cf8f3.d00d12f4337f447cb2d1edda88df192d", uid: "1917762378", image_url: "https://igcdn-photos-a-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-19/11376439_1629117797306440_1800201839_a.jpg", occupation: "Developer", description: "I love street art", twitter_link: "http://twitter.com", instagram_link: "http://instagram.com", linkedin_link: "http://linkedin.com", ello_link: "http://ello.com", facebook_link: "http://facebook.com", dribbble_link: "http://dridbble.com", website_url: "http://www.tracymcaruso.com", admin: true)
+  end
+
   def create_murals
     Mural.create(name: "Mural Name", image:'https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e15/11378549_1440876629552491_805058315_n.jpg', description: 'pretty mural', artist_name: 'megan davis', latitude: 'here', longitude: 'there')
     Mural.create(name: "Mural Name", image:'https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e15/11326820_801827753264739_1573523043_n.jpg', description: 'pretty mural', artist_name: 'megan davis', latitude: 'here', longitude: 'there')
